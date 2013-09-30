a6b4a1bdaac8c64d2a0bd35545b1d87487707028
julian25 <julian25@gmail.com>
2013-09-30 15:57:25 -0400

    now using jetty server

--- project.clj
+++ project.clj
@@ -9,8 +9,10 @@
                  [clj-time "0.6.0"]
                  [cheshire "5.2.0"]
                  [org.clojure/core.memoize "0.5.6"]
-                 [forecast-clojure "1.0.3"]]
+                 [forecast-clojure "1.0.3"]
+                 [ring "1.2.0"]]
   :plugins [[lein-ring "0.8.5"]]
   :ring {:handler weatherbeacon.handler/app}
   :profiles
-  {:dev {:dependencies [[ring-mock "0.1.5"]]}})
+  {:dev {:dependencies [[ring-mock "0.1.5"]]}}
+  :main weatherbeacon.handler)
--- src/weatherbeacon/handler.clj
+++ src/weatherbeacon/handler.clj
@@ -1,7 +1,9 @@
 (ns weatherbeacon.handler
   (:use compojure.core
         [hiccup.page :only (html5)] 
-        [hiccup.middleware :only (wrap-base-url)])
+        [hiccup.middleware :only (wrap-base-url)]
+        [ring.adapter.jetty :only [run-jetty]]
+        [ring.middleware.params :only [wrap-params]])
   (:require [compojure.handler :as handler]
             [compojure.route :as route]
             [compojure.response :as response]
@@ -13,6 +15,18 @@
   (route/resources "/")
   (route/not-found "Page not found"))
 
-(def app
-  (-> (handler/site main-routes)
-      (wrap-base-url)))
+;(def app
+  ;(-> (handler/site main-routes)
+      ;(wrap-base-url)))
+
+;(def app (wrap-params handler/site main-routes))
+
+(def application (handler/site main-routes))
+
+(defn start [port]
+  (run-jetty #'application {:port port :join? false}))
+
+(defn -main []
+  (let [port (Integer/parseInt
+                                 (or (System/getenv "PORT") "8080"))]
+    (start port)))
